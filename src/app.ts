efd550ea56bea650d04f935c7e2d8ba56eeef96e
Lucas Garron <code@garron.net>
2019-10-25 19:18:19 -0700

    Timing.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import ResizeObserver from "resize-observer-polyfill";
 
 (window as any).ResizeObserver = ResizeObserver;
 
-import { Competitor } from "./competitor";
+import { Competitor, Status } from "./competitor";
 
 let initialNumCompetitors = parseFloat(new URL(location.href).searchParams.get("numCompetitors") || "2");
 if (isNaN(initialNumCompetitors)) {
@@ -21,7 +21,11 @@ declare global {
   }
 }
 
-type Phase = "ready" | "scrambling" | "solving";
+enum Phase {
+  Ready = "ready",
+  Scrambling = "scrambling",
+  Solving = "solving"
+}
 type AppState = {
   currentCompetitor: 0,
   phase: Phase
@@ -30,16 +34,15 @@ type AppState = {
 export class FMCDuelApp {
   element: HTMLElement;
   competitorsElem: HTMLElement;
-  state: AppState = {
-    currentCompetitor: 0,
-    phase: "ready"
-  }
+  currentCompetitorIdx: number = 0;
+  phase: Phase = Phase.Ready;
   competitors: Competitor[] = [];
   constructor() {
     this.element = document.querySelector("app");
     this.competitorsElem = this.element.querySelector("competitors");
     document.querySelector("#add-competitor").addEventListener("click", this.addCompetitor.bind(this));
     document.querySelector("#reset").addEventListener("click", this.reset.bind(this));
+    document.querySelector("#start").addEventListener("click", this.startRound.bind(this));
 
     (async () => {
       for (let i = 0; i < initialNumCompetitors; i++) {
@@ -48,14 +51,97 @@ export class FMCDuelApp {
     })();
   }
 
-  async reset() {
+  reset(): void {
     for (const competitor of this.competitors) {
       competitor.reset(initialTimeLimitMs);
     }
+    this.currentCompetitorIdx = 0;
+    this.phase = Phase.Ready;
+  }
+
+  incrementCompetitorIdx(): void {
+    this.currentCompetitorIdx = (this.currentCompetitorIdx + 1) % this.competitors.length
+  }
+
+  competitorDeltaFromCurrent(delta: number): Competitor {
+    return this.competitors[(this.currentCompetitorIdx + this.competitors.length + delta) % this.competitors.length];
+  }
+
+  startRound(): void {
+    if (this.competitors.length < 2) {
+      throw new Error("Need at least two competitors.");
+    }
+    this.reset(); // TODO: Can we eliminate this?
+    this.competitorDeltaFromCurrent(-1).setBeingScrambled();
+    this.competitorDeltaFromCurrent(0).setScrambling();
+    this.phase = Phase.Scrambling;
+    requestAnimationFrame(this.animFrame.bind(this));
+    this.debugLog(0);
+  }
+
+  animFrame(): void {
+    this.competitors.map(c => {
+      switch (c.status) {
+        case Status.Scrambling:
+        case Status.TakingTurn:
+          c.updateTime()
+          break;
+        default:
+          // Nothing
+          break;
+      }
+    });
+    requestAnimationFrame(this.animFrame.bind(this));
+  }
+
+  turnDone(competitorIdx: number): void {
+    if (this.currentCompetitorIdx !== competitorIdx) {
+      console.log("Unexpected competitor index:", competitorIdx);
+      return;
+    }
+
+    switch (this.phase) {
+      case Phase.Scrambling:
+        if (this.currentCompetitorIdx < this.competitors.length - 1) {
+          if (this.competitors.length > 2) {
+            this.competitorDeltaFromCurrent(-1).setWaiting();
+          }
+          this.competitorDeltaFromCurrent(0).setBeingScrambled();
+          this.competitorDeltaFromCurrent(1).setScrambling();
+          this.incrementCompetitorIdx();
+        } else {
+          if (this.competitors.length > 2) {
+            this.competitorDeltaFromCurrent(-1).setWaiting();
+          }
+          this.competitorDeltaFromCurrent(0).setWaiting();
+          this.competitorDeltaFromCurrent(1).setTakingTurn();
+          this.phase = Phase.Solving;
+          this.incrementCompetitorIdx();
+        }
+        break;
+      case Phase.Solving:
+        this.competitorDeltaFromCurrent(0).setWaiting();
+        this.competitorDeltaFromCurrent(1).setTakingTurn();
+        this.incrementCompetitorIdx();
+        break;
+      default:
+        throw new Error("Unimplemented");
+    }
+    this.debugLog(competitorIdx);
+  }
+
+  debugLog(competitorIdx: number) {
+    console.table({
+      phase: this.phase,
+      currentCompetitorIdx: this.currentCompetitorIdx,
+      competitorIdx: competitorIdx,
+      statuses: this.competitors.map(c => c.status),
+      msRemainings: this.competitors.map(c => c.msRemaining)
+    })
   }
 
   async addCompetitor(): Promise<Competitor> {
-    const competitor = new Competitor();
+    const competitor = new Competitor(`Digit${this.competitors.length + 1}`, this.turnDone.bind(this, this.competitors.length));
     this.competitors.push(competitor);
     this.competitorsElem.appendChild(competitor.element);
 
--- src/competitor.ts
+++ src/competitor.ts
@@ -6,29 +6,42 @@ import { Puzzles, KPuzzle } from "cubing/kpuzzle";
 
 const def = Puzzles["333"];
 
+export enum Status {
+  Inactive = "inactive",
+  BeingScrambled = "beingScrambled",
+  Scrambling = "scrambling",
+  Waiting = "waiting",
+  TakingTurn = "takingTurn",
+  Won = "won",
+  Lost = "lost"
+}
+
 export class Competitor {
   moveCounter: number;
   sequence: Sequence;
   puzzle: KPuzzle;
 
   // state
+  status: Status = Status.Inactive;
   msRemaining: number = 0;
   lastRunningStart: number = 0;
-  timeAtLastRunningStart: number = 0;
-  running: boolean = false;
+  msRemainingAtLastRunningStart: number = 0;
 
   element: HTMLElement = document.createElement("competitor");
   connectElem: HTMLElement = document.createElement("button");
   kbElem: HTMLElement = document.createElement("button");
   resetElem: HTMLElement = document.createElement("button");
   counterElem: HTMLElement = document.createElement("counter");
+  timeElem: HTMLElement = document.createElement("span");
   movesElem: HTMLAnchorElement = document.createElement("a");
   // TODO: Let `twisty.js` create its elem.
   twistyElem: HTMLElement = document.createElement("twisty");
 
   twisty: Twisty;
   bluetoothPuzzles: BluetoothPuzzle[] = [];
-  constructor() {
+  private turnDoneHandler = this.turnDone.bind(this);
+
+  constructor(private turnDoneKey: string, private turnDoneCallback: () => void) {
     experimentalShowJumpingFlash(false);
     this.twisty = new Twisty(this.twistyElem, {
       puzzle: def,
@@ -48,20 +61,27 @@ export class Competitor {
     this.connectElem.textContent = "BT";
     this.connectElem.addEventListener("click", this.connect.bind(this, false));
     competitorControlBar.appendChild(this.connectElem);
+
     this.kbElem.textContent = "KB";
     this.kbElem.addEventListener("click", this.connect.bind(this, true));
     competitorControlBar.appendChild(this.kbElem);
+
     this.resetElem.textContent = "Reset";
     this.resetElem.addEventListener("click", this.reset.bind(this));
     competitorControlBar.appendChild(this.resetElem);
+
+    this.timeElem.textContent = "0:00";
+
     competitorControlBar.appendChild(this.counterElem);
+    competitorControlBar.appendChild(this.timeElem);
     competitorControlBar.appendChild(this.movesElem);
     this.movesElem.target = "_blank";
 
     this.element.appendChild(this.twistyElem);
   }
 
-  reset(timeLimitMs: number) {
+  public reset(timeLimitMs: number): void {
+    console.log("reset");
     this.updateMoveCounter(0);
     this.sequence = new Sequence([]);
     this.movesElem.textContent = "";
@@ -69,24 +89,187 @@ export class Competitor {
     this.puzzle = new KPuzzle(def);
 
     this.msRemaining = timeLimitMs;
-    this.running = false;
+    this.displayTime();
+    this.status = Status.Inactive;
     this.twisty.experimentalSetAlg(this.sequence);
   }
 
-  updateMoveCounter(n: number) {
-    this.moveCounter = n;
-    this.counterElem.textContent = this.moveCounter.toString();
-  }
-
-  async connect(keyboard: boolean) {
+  public async connect(keyboard: boolean) {
     console.log("connecting...")
     const bluetoothPuzzle = await (keyboard ? debugKeyboardConnect(this.twisty.experimentalGetPlayer().element) : connect());
-    bluetoothPuzzle.addMoveListener(this.onmove.bind(this));
+    bluetoothPuzzle.addMoveListener(this.onMove.bind(this));
     this.bluetoothPuzzles.push(bluetoothPuzzle);
     console.log("connected!")
   }
 
-  onmove(moveEvent: MoveEvent) {
+  public setWaiting(): void {
+    switch (this.status) {
+      case Status.Inactive:
+      case Status.BeingScrambled:
+      case Status.Scrambling:
+      case Status.TakingTurn:
+        this.stopTimer();
+        this.status = Status.Waiting;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public setScrambling(): void {
+    switch (this.status) {
+      case Status.Inactive:
+      case Status.BeingScrambled:
+      case Status.Waiting:
+        this.startTimer();
+        this.status = Status.Scrambling;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public setBeingScrambled(): void {
+    switch (this.status) {
+      case Status.Inactive:
+      case Status.Waiting:
+      case Status.Scrambling:
+        this.status = Status.BeingScrambled;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public setTakingTurn(): void {
+    switch (this.status) {
+      case Status.Waiting:
+      case Status.Scrambling:
+      case Status.BeingScrambled:
+        this.startTimer();
+        this.status = Status.TakingTurn;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public setWon(): void {
+    switch (this.status) {
+      case Status.TakingTurn:
+      case Status.Waiting:
+        this.stopTimer();
+        this.status = Status.Won;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public setLost(): void {
+    switch (this.status) {
+      case Status.TakingTurn:
+      case Status.Waiting:
+        this.stopTimer();
+        this.status = Status.Lost;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public updateMoveCounter(n: number) {
+    this.moveCounter = n;
+    this.counterElem.textContent = this.moveCounter.toString();
+  }
+
+  private turnDone(event: KeyboardEvent): void {
+    console.log(event.code);
+    if (event.code === this.turnDoneKey) {
+      switch (this.status) {
+        case Status.Scrambling:
+        case Status.TakingTurn:
+          // this.status = Status.Waiting
+          console.log("removing listener", this.turnDoneKey)
+          window.removeEventListener("keydown", this.turnDoneHandler);
+          this.turnDoneCallback();
+          break;
+        // default:
+        //   throw new Error(`Unexpected status! ${this.status}`);
+      }
+    }
+  }
+
+  private listenForTurnDone(): void {
+    console.log("listening", this.turnDoneKey);
+    window.addEventListener("keydown", this.turnDoneHandler)
+  }
+
+  private startTimer(): void {
+    switch (this.status) {
+      case Status.Inactive:
+      case Status.BeingScrambled:
+      case Status.TakingTurn:
+      case Status.Waiting:
+        this.lastRunningStart = Date.now();
+        this.msRemainingAtLastRunningStart = this.msRemaining;
+        console.log({
+          lastRunningStart: this.lastRunningStart,
+          msRemainingAtLastRunningStart: this.msRemainingAtLastRunningStart
+        })
+        this.listenForTurnDone();
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  public updateTime(): void {
+    switch (this.status) {
+      case Status.Scrambling:
+      case Status.TakingTurn:
+        const currentTime = Date.now();
+        this.msRemaining = this.msRemainingAtLastRunningStart - (currentTime - this.lastRunningStart);
+        if (this.msRemaining <= 0) {
+          this.msRemaining = 0;
+          this.status = Status.Lost;
+        }
+        this.displayTime();
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  private displayTime(): void {
+    switch (this.status) {
+      case Status.Inactive:
+      case Status.BeingScrambled:
+      case Status.Scrambling:
+      case Status.TakingTurn:
+      case Status.Lost:
+        this.timeElem.textContent = `${this.msRemaining}`;
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  private stopTimer(): void {
+    switch (this.status) {
+      case Status.Inactive:
+      case Status.BeingScrambled:
+      case Status.Scrambling:
+      case Status.TakingTurn:
+      case Status.Lost:
+        this.updateTime();
+        break;
+      default:
+        throw new Error(`Unexpected status! ${this.status}`);
+    }
+  }
+
+  private onMove(moveEvent: MoveEvent) {
     this.puzzle.applyBlockMove(moveEvent.latestMove);
     // this.svg.draw(def, this.puzzle.state);
 
